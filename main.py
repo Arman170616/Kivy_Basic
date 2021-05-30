fd77e7d8a04db9319352551991db14e3db5630db
H.Arman <64675035+Arman170616@users.noreply.github.com>
2021-05-30 16:01:09 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,59 @@
 
+# This is a sample Python script.
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+
+class MyGridLayout(GridLayout):
+    def __init__(self, **kwargs):
+        #Call grid layout constructor
+        #set columns
+        super(MyGridLayout, self).__init__(**kwargs)
+        self.cols = 2
+
+        #Add widgets
+        self.add_widget(Label(text="Name: "))
+        #Add Input Box
+        self.name = TextInput(multiline=True)
+        self.add_widget(self.name)
+
+        self.add_widget(Label(text="Favorite Pizza: "))
+        # Add Input Box
+        self.pizza = TextInput(multiline=False)
+        self.add_widget(self.pizza)
+
+        self.add_widget(Label(text="Favorite Color: "))
+        # Add Input Box
+        self.color = TextInput(multiline=False)
+        self.add_widget(self.color)
+
+        #create a Submit Button
+        self.submit = Button(text="submit", font_size=32)
+        #Bind the button
+        self.submit.bind(on_press=self.press)
+        self.add_widget(self.submit)
+
+    def press(self, instance):
+        name = self.name.text
+        pizza = self.pizza.text
+        color = self.color.text
+
+        #print(f'Hello {name}, you like {pizza} pizza, and your favourite {color} color')
+        self.add_widget(Label(text=f'Hello {name}, you like {pizza} pizza, and your favourite {color} color'))
+        #clear the input boxes
+        self.name.text = ""
+        self.pizza.text = ""
+        self.color.text = ""
+
+
+class MyApp(App):
+    def build(self):
+        return MyGridLayout()
+
+
+if __name__ == '__main__':
+    MyApp().run()
